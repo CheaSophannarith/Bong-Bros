55d9e60f486a13c580c3e879bebab10cb3523243
LaFii <sophannarith38@gmail.com>
2025-07-19 09:24:32 +0700

    feat: implement ScrollFloat component for animated scrolling text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import SplitText from "./components/SplitText"
+import ScrollFloat from "./components/ScrollFloat";
 
 
 function App() {
 
   return (
-    <>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
       <SplitText
-        text="Hello, Ya Dog!"
+        text="Hello, Rith Dog!"
         className="text-2xl font-semibold text-center"
         delay={100}
         duration={0.6}
@@ -18,8 +19,47 @@ function App() {
         rootMargin="-100px"
         textAlign="center"
       />
-    </>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ height: '100vh' }}>
+      <ScrollFloat
+        animationDuration={1}
+        ease='back.inOut(2)'
+        scrollStart='center bottom+=50%'
+        scrollEnd='bottom bottom-=40%'
+        stagger={0.03}
+      >
+        React Bits
+      </ScrollFloat>
+      </div>
+
+<div style={{ height: '100vh' }}>
+      <ScrollFloat
+        animationDuration={1}
+        ease='back.inOut(2)'
+        scrollStart='center bottom+=50%'
+        scrollEnd='bottom bottom-=40%'
+        stagger={0.03}
+      >
+        Ya
+      </ScrollFloat>
+      </div>
+
+      <div style={{ height: '100vh' }}>
+      <ScrollFloat
+        animationDuration={1}
+        ease='back.inOut(2)'
+        scrollStart='center bottom+=50%'
+        scrollEnd='bottom bottom-=40%'
+        stagger={0.5}
+      >
+        Rith
+      </ScrollFloat>
+      </div>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+
