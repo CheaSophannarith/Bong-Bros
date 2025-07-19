fa1fd0b70d7478c7d7caaff0540ba3024e8e0514
Channsoriya <chann.soriya.1221@rupp.edu.kh>
2025-07-19 10:40:18 +0700

    refactor: remove unused SplitText and ScrollFloat components from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,7 @@
-import SplitText from "./components/SplitText"
-import ScrollFloat from "./components/ScrollFloat";
-
-
 function App() {
-
   return (
     <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
-      <SplitText
-        text="Hello, Rith Dog!"
-        className="text-2xl font-semibold text-center"
-        delay={100}
-        duration={0.6}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        textAlign="center"
-      />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ height: '100vh' }}>
-      <ScrollFloat
-        animationDuration={1}
-        ease='back.inOut(2)'
-        scrollStart='center bottom+=50%'
-        scrollEnd='bottom bottom-=40%'
-        stagger={0.03}
-      >
-        React Bits
-      </ScrollFloat>
-      </div>
-
-<div style={{ height: '100vh' }}>
-      <ScrollFloat
-        animationDuration={1}
-        ease='back.inOut(2)'
-        scrollStart='center bottom+=50%'
-        scrollEnd='bottom bottom-=40%'
-        stagger={0.03}
-      >
-        Ya
-      </ScrollFloat>
-      </div>
-
-      <div style={{ height: '100vh' }}>
-      <ScrollFloat
-        animationDuration={1}
-        ease='back.inOut(2)'
-        scrollStart='center bottom+=50%'
-        scrollEnd='bottom bottom-=40%'
-        stagger={0.5}
-      >
-        Rith
-      </ScrollFloat>
-      </div>
-      </div>
+      <h1>Hello World</h1>
     </div>
   )
 }
